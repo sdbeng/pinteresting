64957ff31f87df685d7815d60c106e30df7ff12e
Sergio Barrera <sdbeng@gmail.com>
2013-11-17 20:38:47 -0800

    Added new pins scaffold resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108050608) do
+ActiveRecord::Schema.define(version: 20131117215135) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
